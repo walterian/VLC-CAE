fd8c33d69c99332a46e639e3097534cc3f1d480d
walterian <ianwalter.college@gmail.com>
2019-04-04 12:17:57 -0400

    Split Decoder and Channel

--- EncDec.py
+++ EncDec.py
@@ -32,9 +32,7 @@ i = 0
 while(i<M):
     rawdata[i] = i
     i += 1
-
 data = to_categorical(rawdata)
-#from numpy import reshape
 data = data.astype(int)
 ydata = np.reshape(data, (data.shape[0], 1, 1, data.shape[1]))
 yvaldata = ydata
@@ -47,7 +45,8 @@ xvaldata = xdata
 def Encoder():
     input_img = Input(shape=(1, 1, M))
     e1 = Dense(M, activation='relu')(input_img)     # input layer, output size M
-    e2 = Dense(16*L*L, activation='relu')(e1)
+    e11 = BatchNormalization()(e1)
+    e2 = Dense(16*L*L, activation='relu')(e11)
     e3 = BatchNormalization()(e2)
     e4 = Reshape((4*L, 4*L, -1))(e3)
     e5 = Conv2D(M, kernel_size=(3, 3), padding='same', activation='relu')(e4)          # Conv_1, M 3x3 filters
@@ -59,15 +58,16 @@ def Encoder():
     e11 = Conv2D(1, kernel_size=(3, 3), padding='same')(e10)      # Conv_3, 1 3x3 filters 
     sigmoid = Lambda(lambda x: 1/(1 + 2.718281 ** (-x*delta)))(e11)
     return Model(input_img, sigmoid)
-# End Encoder layer
-
-def Decoder():
+def Channel():
     input_img = Input(shape=(L, L, 1))
     d01 = UpSampling2D(size=(2,2))(input_img)
-    d02 = ZeroPadding2D(padding=4)(d01)
+    d02 = ZeroPadding2D(padding=9)(d01)
     d03 = GaussianNoise(stddev=0.1)(d02)        # add noise to channel simulation
-# Start Decoder
-    d1 = Conv2D(2*M, kernel_size=(5, 5), padding='same', activation='relu')(d03)       # Conv_4, 2M 5x5 filters
+    return Model(input_img, d03)
+def Decoder():
+    # Start Decoder
+    input_enc = Input(shape=(28, 28, 1))
+    d1 = Conv2D(2*M, kernel_size=(5, 5), padding='same', activation='relu')(input_enc)       # Conv_4, 2M 5x5 filters
     d2 = BatchNormalization()(d1)
     d3 = MaxPooling2D()(d2)
     d4 = Conv2D(2*M, kernel_size=(3, 3), padding='same', activation='relu')(d3)       # Conv_5, 2M 3x3 filters
@@ -77,50 +77,54 @@ def Decoder():
     d8 = Dense(M, activation='relu')(d7)
     d9 = BatchNormalization()(d8)
     d10 = Dense(M, activation='softmax')(d9)                 # output layer, output size M
-    return Model(input_img, d10)
+    return Model(input_enc, d10)
 
 x = Input(shape=(1, 1, M))
-callbacks = [EarlyStopping(monitor='loss', patience=50),
-             ModelCheckpoint(filepath='saved_weights.h5', monitor='loss', save_best_only=True, save_weights_only=True),
+# Define callbacks to be monitored during training time
+callbacks = [EarlyStopping(monitor='val_loss', patience=50),
+             ModelCheckpoint(filepath='saved_weights.h5', monitor='val_loss', save_best_only=True, save_weights_only=True),
              History()]
-
 adam = keras.optimizers.Adam(clipnorm=1., clipvalue=0.5, amsgrad=True)   # clipnorm is necessary to prevent gradients from blowing up (weights = NaN)
 
 for delta in np.arange(1, 4):
     print('delta = ', delta)
-    cae = Model(x, Decoder()(Encoder()(x)))
-
-    # model train
+    cae = Model(x, Decoder()(Channel()(Encoder()(x))))
+    # model training
     cae.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
     if delta != 1:
         cae.load_weights('saved_weights.h5')
-    
     cae.fit(xdata, ydata, epochs=numepochs, validation_data=(xvaldata, yvaldata), batch_size=batchsize, callbacks=callbacks)
+# Issue here is that training fails when delta > 3, so possible solution is to
+# train up to delta = 3, then save weights -> create network with delta = 10 or 100 etc. and load weights
+# then just use that network; it's far from ideal but could get us OOK
 
+def Encoder_Test():
+    input_img = Input(shape=(1, 1, M))
+    e1 = Dense(M, activation='relu')(input_img)     # input layer, output size M
+    e11 = BatchNormalization()(e1)
+    e2 = Dense(16*L*L, activation='relu')(e11)
+    e3 = BatchNormalization()(e2)
+    e4 = Reshape((4*L, 4*L, -1))(e3)
+    e5 = Conv2D(M, kernel_size=(3, 3), padding='same', activation='relu')(e4)          # Conv_1, M 3x3 filters
+    e6 = BatchNormalization()(e5)
+    e7 = MaxPooling2D()(e6)
+    e8 = Conv2D(2*M, kernel_size=(3, 3), padding='same', activation='relu')(e7)       # Conv_2, 2M 3x3 filters
+    e9 = BatchNormalization()(e8)
+    e10 = MaxPooling2D()(e9)
+    e11 = Conv2D(1, kernel_size=(3, 3), padding='same')(e10)      # Conv_3, 1 3x3 filters 
+    sigmoid = Lambda(lambda x: 1/(1 + 2.718281 ** (-x*1000)))(e11)
+    return Model(input_img, sigmoid)
 
-"""numepochs = 25
-for delta in [10, 100]:
-
-    encoder = cae.layers[1]
-    predicted = encoder.predict(xdata)
-
-    print('delta = ', delta)
-    cae = Model(x, Decoder()(Encoder()(x)))
-
-    # model train
-    cae.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    if delta != 1:
-        cae.load_weights('saved_weights.h5')
-    
-    
-    cae.fit(xdata, ydata, epochs=numepochs, validation_data=(xvaldata, yvaldata), batch_size=batchsize, callbacks=callbacks)
-    cae
-    cae.save_weights('saved_weights.h5')"""
-
+cae = Model(x, Decoder()(Channel()(Encoder_Test()(x))))
+cae.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+cae.load_weights('saved_weights.h5')
+cae.summary()
 cae.save('trained_cae.h5')
 
 encoder = cae.layers[1]
-decoder = cae.layers[2]
+decoder = cae.layers[3]
+encoder.summary()
+decoder.summary()
 encoder.save_weights('encoder_weights.h5')
 decoder.save_weights('decoder_weights.h5')
 encoder.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
