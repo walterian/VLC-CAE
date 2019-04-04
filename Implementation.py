f5586b0ae3620fe3ef17a26202dcd3037061d7f4
walterian <ianwalter.college@gmail.com>
2019-04-03 21:36:31 -0400

    Worked on parameterized sigmoid - max delta = 3

---
+++ Implementation.py
@@ -0,0 +1,2 @@
+# This file contains an attempt at actually putting the network trained in EncDec.py to practice
+
--- Testmatch.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-for delta in np.arange(1, 101):
-    x = np.arange(-1,1,0.01)
-
-    y = 1/(1 + 2.718281 ** (-x*delta))
-    print(y[:])
