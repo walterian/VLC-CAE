4be5cb981fadc7d9ce72da15a528f6976cd3ba2f
walterian <ianwalter.college@gmail.com>
2019-04-06 08:43:39 -0400

    Implemented test standard deviation

--- EncDec.py
+++ EncDec.py
@@ -1,32 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-import tensorflow
-
 import keras
-
-from keras.utils import plot_model
-from keras.utils import to_categorical
-from keras.models import Sequential, Model, load_model
-from keras.layers import Dense, Activation, Flatten, Reshape, Input, Lambda
-from keras.layers import Conv2D, Dropout, MaxPooling2D, UpSampling2D, ZeroPadding2D
-from keras.layers import BatchNormalization, GaussianNoise
-from keras.callbacks import History, EarlyStopping, ModelCheckpoint
-
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow
+from keras.callbacks import EarlyStopping, History, ModelCheckpoint
+from keras.layers import (Activation, BatchNormalization, Conv2D, Dense,
+                          Dropout, Flatten, GaussianNoise, Input, Lambda,
+                          MaxPooling2D, Reshape, UpSampling2D, ZeroPadding2D)
+from keras.models import Model, Sequential, load_model
+from keras.utils import plot_model, to_categorical
 from numpy import array
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 # definitions
 M = 64
 L = 5
-numepochs = 100
-delta = 1
+numepochs = 200
 batchsize = 32
+SNR = 100
 
-
-# One hot encoding
+# One hot encoding/data generation
 rawdata = np.zeros(M)
 i = 0
 while(i<M):
@@ -38,9 +30,6 @@ ydata = np.reshape(data, (data.shape[0], 1, 1, data.shape[1]))
 yvaldata = ydata
 xdata = np.reshape(data, (data.shape[0], 1, 1, data.shape[1]))
 xvaldata = xdata
-# Define Structure
-#input_shape = data.shape
-
 
 def Encoder():
     input_img = Input(shape=(1, 1, M))
@@ -62,7 +51,7 @@ def Channel():
     input_img = Input(shape=(L, L, 1))
     d01 = UpSampling2D(size=(2,2))(input_img)
     d02 = ZeroPadding2D(padding=9)(d01)
-    d03 = GaussianNoise(stddev=0.1)(d02)        # add noise to channel simulation
+    d03 = GaussianNoise(stddev=np.sqrt(1/(2*10**(SNR/10))))(d02)        # add noise to channel simulation
     return Model(input_img, d03)
 def Decoder():
     # Start Decoder
@@ -81,7 +70,7 @@ def Decoder():
 
 x = Input(shape=(1, 1, M))
 # Define callbacks to be monitored during training time
-callbacks = [EarlyStopping(monitor='val_loss', patience=50),
+callbacks = [EarlyStopping(monitor='val_loss', patience=100),
              ModelCheckpoint(filepath='saved_weights.h5', monitor='val_loss', save_best_only=True, save_weights_only=True),
              History()]
 adam = keras.optimizers.Adam(clipnorm=1., clipvalue=0.5, amsgrad=True)   # clipnorm is necessary to prevent gradients from blowing up (weights = NaN)
@@ -119,7 +108,7 @@ cae = Model(x, Decoder()(Channel()(Encoder_Test()(x))))
 cae.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
 cae.load_weights('saved_weights.h5')
 cae.summary()
-cae.save('trained_cae.h5')
+cae.save('Trained/trained_cae_snr_'+str(SNR)+'.h5')
 
 encoder = cae.layers[1]
 decoder = cae.layers[3]
@@ -134,12 +123,7 @@ decoder.load_weights('decoder_weights.h5')
 encoder.save('trained_encoder.h5')
 decoder.save('trained_decoder.h5')
 
-
-
-
 predicted = encoder.predict(xdata)
-
-import matplotlib.pyplot as plt
 
 # Plot training & validation accuracy values
 plt.plot(callbacks[2].history['acc'])
@@ -158,8 +142,3 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
-
-
-import vis
-from vis.utils import utils
-from vis.visualization import visualize_saliency
--- Implementation.py
+++ Implementation.py
@@ -1,2 +1,18 @@
 # This file contains an attempt at actually putting the network trained in EncDec.py to practice
+import keras
+import numpy as np
+from keras.models import Model, load_model
 
+model = load_model('trained_cae.h5')
+encoder = load_model('trained_encoder.h5')
+decoder = load_model('trained_decoder.h5')
+
+""" So variance is related to the physical property of noise power
+"""
+
+# SNR is in db (20log10(S/N))
+SNR = 20
+
+Beta = np.sqrt(1/(2*10**(SNR/20)))
+
+print(Beta)
